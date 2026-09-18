b56250b653a10f699db43d0449ee2cd42aae0771
baveryk <io.kainama@gmail.com>
2026-09-18 23:48:03 +0700

    Finished exercise 2

--- routed-anecdotes/src/components/CreateNew.jsx
+++ routed-anecdotes/src/components/CreateNew.jsx
@@ -19,6 +19,13 @@ const CreateNew = ({ addNew }) => {
     navigate("/")
   }
 
+  const handleReset = (e) => {
+    e.preventDefault()
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -41,7 +48,8 @@ const CreateNew = ({ addNew }) => {
             {...info}
           />
         </div>
-        <button>create</button>
+        <button type="submit">create</button>
+        <button onClick={handleReset}>reset</button>
       </form>
     </div>
   )
